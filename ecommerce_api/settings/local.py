76f5ea1838399ab8fc134eeac49ad6b0cdfe7db8
amirhossein-moloki <amirh.moloki@gmail.com>
2025-07-04 15:22:56 +0300

    Added http://localhost:3000 to CORS ALLOWED ORIGIONS

--- ecommerce_api/settings/local.py
+++ ecommerce_api/settings/local.py
@@ -4,5 +4,8 @@ DEBUG = True
 # DATABASES['default']['HOST'] = 'localhost'
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
