951f41c11f664121cc229bde5023223de11dfe29
amirhossein-moloki <amirh.moloki@gmail.com>
2025-12-28 23:26:41 +0330

    Make debug toolbar optional in dev settings

--- ecommerce_api/settings/development.py
+++ ecommerce_api/settings/development.py
@@ -1,7 +1,14 @@
-from .base import INSTALLED_APPS
+import importlib.util
+
+from .base import INSTALLED_APPS, MIDDLEWARE
 
 # Development-specific settings
-INSTALLED_APPS += [
-    "debug_toolbar",
-    "django_extensions",
-]
+INSTALLED_APPS += ["django_extensions"]
+
+if importlib.util.find_spec("debug_toolbar"):
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE = [
+        *MIDDLEWARE[:2],
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+        *MIDDLEWARE[2:],
+    ]
--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -114,7 +114,7 @@ urlpatterns = [
     path("", include("integrations.urls", namespace="integrations")),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
     from debug_toolbar.toolbar import debug_toolbar_urls
 
     urlpatterns += debug_toolbar_urls()
